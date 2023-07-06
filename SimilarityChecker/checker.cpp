6952544c98e5347e349f69377144c0fb91b6ae86
hj41790 <hj41790@naver.com>
2023-07-06 11:18:22 +0900

    [length] get zero point : refactoring

--- SimilarityChecker/checker.cpp
+++ SimilarityChecker/checker.cpp
@@ -5,9 +5,17 @@ using namespace std;
 class SimilarityChecker
 {
 public:
+	bool isDoubleLength(const string& input1, const string& input2)
+	{
+		return input1.length() == input2.length() * 2 ||
+			input2.length() == input1.length() * 2;
+	}
+
 	int getLengthPoint(const string& input1, const string& input2)
 	{
 		if (input1.length() == input2.length()) return 60;
+
+		if (isDoubleLength(input1, input2)) return 0;
 
 		return 0;
 	}
